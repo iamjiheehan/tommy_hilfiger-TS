c834a2b960da462fa832634ace0a711a69641126
iamjiheehan <hatrix1014@gmail.com>
2023-09-20 15:52:29 +0900

    Updated youtube for Magazine section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -618,13 +618,7 @@ function Main() {
                         <Styles.MagazineWrap>
                             <div className="left no-product">
                                 <Styles.NewTitle className="title-wrap">
-                                    <h2 className="title">FOCUS ON</h2>
-                                    <div className="right-box">
-                                        <a href="#!" className="btn-more-view type-dark-blue">
-                                            <span className="text">23FW CAMPAIGN</span>
-                                            <i className="more-arr-dark-white"></i>
-                                        </a>
-                                    </div>
+                                    <h2 className="title">23FW CAMPAIGN</h2>
                                 </Styles.NewTitle>
                                 <div className="sub-title-wrap">
                                     <span className="title">23'FW NEW VIDEO <br /> TOMMY HILFIGER</span>
@@ -637,16 +631,16 @@ function Main() {
                                         <div class="vod-box-back">
                                             <img src={vidBack} alt="대체텍스트" />
                                         </div>
-                                        <iframe
-                                            className="vod-box"
-                                            frameborder="0"
-                                            allowfullscreen="1"
-                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                            title="TOMMY JEANS 23 FALL CAMPAIGN"
-                                            width="100%"
-                                            height="100%"
-                                            src="https://www.youtube.com/embed/k8Z5M8n8RJA?autoplay=false&amp;rel=0&amp;playsinline=1&amp;mute=1&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fwww.hfashionmall.com&amp;widgetid=3"
-                                        ></iframe>
+                                        <iframe 
+                                            className="vod-box" 
+                                            frameborder="0" 
+                                            allowfullscreen="1" 
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                            title="TOMMY HILFIGER 23FALL STORY" 
+                                            width="100%" 
+                                            height="100%" 
+                                            src="https://www.youtube.com/embed/lIacQpfZyy0?autoplay=false&amp;rel=0&amp;playsinline=1&amp;mute=1&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fwww.hfashionmall.com&amp;widgetid=1">
+                                        </iframe>
                                     </div>
                                     <script>
                                         {`

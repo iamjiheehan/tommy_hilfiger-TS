ae828494fca33ee56de37dc289d9c20fcd52c65a
iamjiheehan <hatrix1014@gmail.com>
2023-09-22 11:50:35 +0900

    Updated Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,16 +2,101 @@ import React from 'react';
 
 import * as Styles from './FooterStyle';
 
+
 function Footer() {
     return (
         <>
             <Styles.FooterWrap>
                 <div className="inner">
                     <p className="logo">현대백화점 그룹 공식 패션몰</p>
-                    <div className="familysite"></div>
-                    <div className="link"></div>
-                    <div className="info"></div>
-                    <div className="copy"></div>
+                    <Styles.FamilySite>
+                        <button type="button" className="btn">FAMILY SITE</button>
+                        <div className="list">
+                            <ul>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">더현대닷컴</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대백화점 DUTY FREE</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">e슈퍼마켓</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대Hmall</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대리바트몰</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대렌탈케어</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">더한섬닷컴</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">EQL</a></li>
+                            </ul>
+                            <p>
+                                관계사 사이트
+                            </p>
+                            <ul>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대백화점그룹</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대백화점</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대그린푸드</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대홈쇼핑</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대리바트몰</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">㈜현대H&amp;S</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대렌탈케어</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대드림투어</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">에버다임</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">씨엔에스푸드시스템</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대케이터링시스템</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대IT&amp;E</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">현대이지웰</a></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">한섬</a></li>
+                            </ul>
+                        </div>
+                    </Styles.FamilySite>
+                    <Styles.Link>
+                        <div className="box">
+                            <p className="title">ABOUT</p>
+                            <ul className="list">
+                                <li><a href="#!">회사소개</a></li>
+                                <li><a href="#!">매장안내</a></li>
+                                <li><a href="#!">고객센터</a></li>
+                                <li><button type="button">이용약관</button></li>
+                                <li><a href="#!" target="_blank" title="새창으로 열림">ESG</a></li>
+                            </ul>
+                        </div>
+                        <div className="box">
+                            <p className="title">HELP</p>
+                            <ul className="list">
+                                <li>
+                                    <a href="#!">회원혜택</a>
+                                </li>
+                                <li><a href="#!">한섬 VVIP 혜택</a></li>
+                                <li><button><strong class="privacy">개인정보처리방침</strong></button></li>
+                                <li><a href="#!">고객의 소리</a></li>
+                            </ul>
+                        </div>
+                        <div class="sns">
+                            <ul>
+                                <li className="facebook"><a href="#!" target="_blank">Facebook</a></li>
+                                <li className="post"><a href="#!" target="_blank">Post</a></li>
+                                <li className="insta"><a href="#!" target="_blank">Instagram</a></li>
+                            </ul>
+                        </div>
+                    </Styles.Link>
+                    <Styles.Info>
+                        <p>(주)한섬<br />서울특별시 강남구 도산대로 523(청담동)
+                            <br />대표이사 : 김민덕
+                            <br />사업자등록번호 : 120-81-26337
+                            <br />통신판매업신고 : 제 2009-서울강남-00826 호
+                            <br />개인정보보호책임자 : 윤인수
+                            <br />호스팅서비스 : (주)한섬
+                            <a href="#!" target="_blank">사업자정보확인</a>
+                            <button>채무지급보증안내</button>
+                        </p>
+                        <p className="contact">
+                            <span>전화 1800-5700(유료)</span>
+                            <span>팩스 02-476-8169</span>
+                            <span>이메일
+                                <a href="#!">hfashionmall@hyundaihmall.com</a>
+                            </span>
+                        </p>
+                    </Styles.Info>
+                    <Styles.Copy>
+                        <small>COPYRIGHT 2023 HANDSOME CO.LTD.ALL RIGHTS RESERVED</small>
+                        <p className="brand">
+                            H FASHION은 루즈앤라운지, SJYP, 타미힐피거, CK캘빈클라인, DKNY, CLUB MONACO, TOM GREYHOUND, FOURM THE STORE, FOURM STUDIO, FOURM MEN'S LOUNGE, MUE,<br /> OUR LEGACY, ASPESI, VERONICA BEARD, MOOSE KNUCKLES, BALLY, LANVIN PARIS, 3.1 PHILLIP LIM의 공식 온라인몰이며, 정품만 취급합니다.
+                        </p>
+                    </Styles.Copy>
                 </div>
             </Styles.FooterWrap>
         </>
--- src/components/Footer/FooterStyle.js
+++ src/components/Footer/FooterStyle.js
@@ -1,12 +1,187 @@
 import styled from "styled-components";
+import logo from "../../assets/images/img_logo_footer.png";
+import familylogo from "../../assets/images/icon_familysite_arrow.png";
+import sns from "../../assets/images/icon_footer_sns.png";
 
 export const FooterWrap = styled.div`
-    font-size: 13px;
+    * {
+        font-size: 13px !important;
+    }
     font-weight: 500;
     background: #000;
+
     .inner {
         min-width: 0;
         padding: 100px 80px;
 
+        position: relative;
+        max-width: 1920px;
+        margin: 0 auto;
+    }
+
+    .inner .logo {
+        width: 278px;
+        height: 30px;
+        background: url(${logo}) no-repeat;
+        font-size: 0;
+    }
+`;
+
+export const FamilySite = styled.div`
+    position: absolute;
+    z-index: 95;
+    top: 100px;
+    right: 80px;
+
+    .btn {
+        position: relative;
+        width: 165px;
+        height: 30px;
+        color: #fff;
+        text-align: left;
+    }
+
+    .btn::after {
+        content: "";
+        position: absolute;
+        color: #fff;
+        top: 0;
+        right: 0;
+        width: 30px;
+        height: 30px;
+        background: url(${familylogo}) no-repeat 50% 50%;
+    }
+
+    .list {
+        display: none;
+        position: absolute;
+        right: 0;
+        bottom: 30px;
+        width: 165px;
+        padding: 15px;
+        background: #fff;
+        border: 1px solid #fafafa;
+    }
+
+    .list p {
+        font-weight: 700;
+        line-height: 1.5;
+    }
+
+    .list p ~ p {
+        margin-top: 10px;
+    }
+
+    .list ul {
+        margin: 5px 0 0;
+    }
+
+    .list ul > li {
+        line-height: 1.5;
+        font-size: 12px;
+    }
+`;
+
+export const Link = styled.nav`
+    position: absolute;
+    top: 185px;
+    right: 80px;
+    color: #666;
+
+    a {
+        color: #666;
+    }
+    .box {
+        float: left;
+        width: 165px;
+    }
+
+    .title {
+        color: #fff;
+        font-weight: 700;
+    }
+
+    .list {
+        margin: 10px 0 0;
+        color: #666;
+    }
+
+    .list li {
+        line-height: 1.7;
+    }
+
+    .sns {
+        position: absolute;
+        top: 150px;
+        right: 0;
+        width: 165px;
+        color: #666;
+    }
+
+    .sns li {
+        position: relative;
+        margin: 0 0 8px;
+        padding-left: 30px;
+        line-height: 24px;
+    }
+
+    .sns li:after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 24px;
+        height: 24px;
+        background: url(${sns}) no-repeat;
+    }
+
+    .sns li.post:after {
+        background-position: -24px 0;
+    }
+
+    .sns li.insta:after {
+        background-position: -48px 0;
+    }
+`;
+
+export const Info = styled.div`
+    margin-top: 40px;
+    color: #666;
+    line-height: 1.7;
+
+    color: #666 !important;
+
+    a,
+    button {
+        margin-left: 20px;
+        color: #666 !important;
+        font-weight: normal !important;
+    }
+    .contact {
+        margin-top: 15px;
+    }
+
+    .contact span {
+        margin-right: 20px;
+    }
+
+    .contact span > a {
+        margin-left: 0;
+    }
+`;
+
+export const Copy = styled.div`
+    margin: 40px 0 0;
+
+    small {
+        color: #666;
+        letter-spacing: 4px;
+        font-size: 10px !important;
+    }
+
+    .brand {
+        margin: 40px 0 0;
+        color: #666;
+        line-height: 1.5;
     }
 `;

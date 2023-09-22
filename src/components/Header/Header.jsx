8b3ee65b44b4e2dd674ca4a80fe1e3fa3edd2979
iamjiheehan <hatrix1014@gmail.com>
2023-09-22 14:59:37 +0900

    Updated newBox menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -29,41 +29,271 @@ function Header() {
                         </div>
                     </Styles.MainFlex>
                     <Styles.GnbWrap className="header-gnb">
-                        <Styles.BtnList >
+                        <Styles.BtnWrap >
                             <button className='btn-list' type="button"><span>카테고리</span></button>
                             <button className='btn-list' marginright="0" type="button"><span>브랜드</span></button>
-                        </Styles.BtnList>
-                        <ul>
-                            <li className="on">
-                                <a href="#!">MAIN</a>
-                            </li>
-                            <li className="">
-                                <a href="!">
-                                    MEN</a>
-                            </li>
-                            <li className="">
-                                <a href="#!">
-                                    WOMEN</a>
-                            </li>
-                            <li className="">
-                                <a href="#!">
-                                    TOMMY JEANS</a>
-                            </li>
-                            <li className="">
-                                <a href="#!">
-                                    KIDS</a>
-                            </li>
-                            <li className="">
-                                <a href="#!">
-                                    SHOES</a>
-                            </li>
-                            <li className="">
-                                <a href="#!">
-                                    GOLF</a>
-                            </li>
-                        </ul>
+                        </Styles.BtnWrap>
+                        <div className='header-main-gnb'>
+                            <ul>
+                                <li className="on">
+                                    <a href="#!">MAIN</a>
+                                </li>
+                                <li className="">
+                                    <a href="!">
+                                        MEN</a>
+                                </li>
+                                <li className="">
+                                    <a href="#!">
+                                        WOMEN</a>
+                                </li>
+                                <li className="">
+                                    <a href="#!">
+                                        TOMMY JEANS</a>
+                                </li>
+                                <li className="">
+                                    <a href="#!">
+                                        KIDS</a>
+                                </li>
+                                <li className="">
+                                    <a href="#!">
+                                        SHOES</a>
+                                </li>
+                                <li className="">
+                                    <a href="#!">
+                                        GOLF</a>
+                                </li>
+                            </ul>
+                        </div>
                     </Styles.GnbWrap>
                 </div>
+                <Styles.NewBox>
+                    <div className="gnb-new-box">
+                        <div className="menu-contents">
+                            <div className="brand-cate-wrap">
+                                <ul className="brand-cate-list">
+                                    <li>
+                                        <a href="#!">
+                                            여성</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            EXCLUSIVE</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            니트/스웨터</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            티셔츠</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            셔츠/블라우스</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            아우터</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            팬츠</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            데님</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            언더웨어</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            원피스</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            스커트</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            가방</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            신발</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            악세서리</a>
+                                    </li>
+                                </ul>
+                                <ul className="brand-cate-list">
+                                    <li>
+                                        <a href="#!">
+                                            남성</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            EXCLUSIVE</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            티셔츠</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            셔츠</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            니트/스웨터</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            아우터</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            팬츠</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            데님</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            수트정장</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            언더웨어</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            가방</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            신발</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            악세서리</a>
+                                    </li>
+                                </ul>
+                                <ul className="brand-cate-list">
+                                    <li>
+                                        <a href="#!">
+                                            키즈</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            EXCLUSIVE</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            남아</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            여아</a>
+                                    </li>
+                                </ul>
+                                <ul className="brand-cate-list">
+                                    <li>
+                                        <a href="#!">
+                                            잡화</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            EXCLUSIVE</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            모자</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            여성신발</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            여성가방</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            여성패션잡화</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            남성신발</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            남성가방</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            남성패션잡화</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            키즈패션잡화</a>
+                                    </li>
+                                </ul>
+                                <ul className="brand-cate-list">
+                                    <li>
+                                        <a href="#!;">
+                                            라이프스타일</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            펫</a>
+                                    </li>
+                                </ul>
+                                <ul className="brand-cate-list">
+                                    <li>
+                                        <a href="#!">
+                                            골프</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            남성 아우터</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            남성 상의</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            남성 하의</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            여성 아우터</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            여성 상의</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            여성 하의</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            골프 악세서리</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            골프 가방</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </Styles.NewBox>
             </header>
         </>
     );
--- src/components/Header/HeaderStyle.js
+++ src/components/Header/HeaderStyle.js
@@ -46,9 +46,9 @@ export const Absolute = styled.div`
     }
 `;
 
-export const BtnList = styled.div`
+export const BtnWrap = styled.div`
     position: absolute;
-    left: 80px;
+    /* left: 80px; */
 
     span {
         font-size: 15px;
@@ -56,6 +56,8 @@ export const BtnList = styled.div`
         color: #00174f;
     }
 
+
+    
     .btn-list {
         cursor: pointer;
 
@@ -63,6 +65,20 @@ export const BtnList = styled.div`
         display: inline-block;
         padding-bottom: 16px;
         margin-right: ${(props) => props.marginright || "30px"};
+    
+        position: relative;
+    }
+
+    .btn-list::after {
+        content: "";
+        display: block;
+        width: 100%;
+        height: 2px;
+        background: #00174f;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        top:1px;
     }
 `;
 
@@ -82,16 +98,16 @@ export const MainFlex = styled.div`
         font-size: 13px;
         vertical-align: middle;
 
-        &::after{
+        &::after {
             content: "";
             position: absolute;
             top: 3px;
             left: 0;
             height: 10px;
-            border-left : 1px solid #000;
+            border-left: 1px solid #000;
         }
 
-        &:first-of-type::after{
+        &:first-of-type::after {
             opacity: 0;
         }
     }
@@ -125,17 +141,70 @@ export const GnbWrap = styled.div`
     max-width: 1920px;
     margin: auto;
 
-    ul {
+    position: relative;
+
+    .header-main-gnb ul {
         display: flex;
         justify-content: center;
         margin: auto;
         list-style: none;
     }
 
-    ul > li {
+    .header-main-gnb ul > li {
         display: block;
         margin: 0 5px;
         padding: 0 10px;
         letter-spacing: 0;
+    }
+`;
+
+export const NewBox = styled.div`
+
+
+        position: absolute;
+        left: 0;
+        right: 0;
+        background: #fff;
+        z-index: 10;
+
+    .gnb-new-box {
+        min-width: 1366px;
+        max-width: 1920px;
+        margin: auto;
+        position: relative;
+        padding: 0 80px;
+
+    }
+
+    .gnb-new-box .menu-contents {
+        padding: 40px 0;
+    }
+
+    .gnb-new-box .menu-contents .brand-cate-wrap {
+        display: flex;
+        justify-content: flex-start;
+        align-items: flex-start;
+    }
+
+    .gnb-new-box .menu-contents .brand-cate-wrap .brand-cate-list {
+        width: 150px;
+        margin-right: 20px;
+    }
+
+    .gnb-new-box .menu-contents .brand-cate-wrap .brand-cate-list li {
+        /* padding-bottom: 16px; */
+        padding-bottom: 8px;
+    }
+
+    .gnb-new-box .menu-contents .brand-cate-wrap .brand-cate-list li a {
+        font-size: 13px;
+        color: var(--themeTextColor);
+        font-weight: 400;
+        outline: none;
+    }
+
+    .gnb-new-box .menu-contents .brand-cate-wrap .brand-cate-list li:first-of-type a {
+        font-weight: 600;
+        
     }
 `;

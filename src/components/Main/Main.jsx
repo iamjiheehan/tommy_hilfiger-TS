7719736306cf7b7fa7828da76346d7344625c89c
한지희, Jihee Han <hatrix1014@gmail.com>
2023-09-26 00:39:56 +0900

    Updated Json datas

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -311,62 +311,6 @@ function Main() {
                                         </li>
                                     ))}
                                 </ul>
-                                {/* <ul className="swiper-wrapper">
-                                    <li className="swiper-item">
-                                        <figure className="item-box">
-                                            <div className="item-img">
-                                                <div className="img-box">
-                                                    <a href="#!">
-                                                        <div className="img"><img src="https://cdn.hfashionmall.com/goods/THBR/23/08/03/GM0123080322170_1_ORGINL_1691130517534.jpg?RS=600x600&amp;AR=0&amp;CS=400x600" alt="폴로 칼라 바시티 재킷" /></div>
-                                                    </a>
-                                                </div>
-                                                <button type="button" className="btn-like"></button>
-                                                <button type="button" className="btn-quick"></button>
-                                            </div>
-                                            <figcaption className="item-info">
-                                                <a href="#!">
-                                                    <div className="item-brand">TOMMY HILFIGER WOMEN</div>
-                                                    <div className="item-name">폴로 칼라 바시티 재킷</div>
-                                                    <div className="item-opt">
-                                                        <div className="item-price">
-                                                            <span className="price">310,500</span>
-                                                            <del className="regular">345,000</del>
-                                                            <span className="percent">10%</span>
-                                                        </div>
-                                                        <div className="item-label"></div>
-                                                    </div>
-                                                </a>
-                                            </figcaption>
-                                        </figure>
-                                    </li>
-                                    <li className="swiper-item">
-                                        <figure className="item-box">
-                                            <div className="item-img">
-                                                <div className="img-box">
-                                                    <a href="#!">
-                                                        <div className="img"><img src="https://cdn.hfashionmall.com/goods/THBR/23/08/03/GM0123080322170_1_ORGINL_1691130517534.jpg?RS=600x600&amp;AR=0&amp;CS=400x600" alt="폴로 칼라 바시티 재킷" /></div>
-                                                    </a>
-                                                </div>
-                                                <button type="button" className="btn-like"></button>
-                                                <button type="button" className="btn-quick"></button>
-                                            </div>
-                                            <figcaption className="item-info">
-                                                <a href="#!">
-                                                    <div className="item-brand">TOMMY HILFIGER WOMEN</div>
-                                                    <div className="item-name">폴로 칼라 바시티 재킷</div>
-                                                    <div className="item-opt">
-                                                        <div className="item-price">
-                                                            <span className="price">310,500</span>
-                                                            <del className="regular">345,000</del>
-                                                            <span className="percent">10%</span>
-                                                        </div>
-                                                        <div className="item-label"></div>
-                                                    </div>
-                                                </a>
-                                            </figcaption>
-                                        </figure>
-                                    </li>
-                                </ul> */}
                                 <div className="slide-nav">
                                     <button className="slide-nav-prev button-disabled">
                                         <span className="offscreen"></span>

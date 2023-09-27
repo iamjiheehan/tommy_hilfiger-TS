ef5fefcb5e419ed73b375002d7dde9abd000931b
iamjiheehan <hatrix1014@gmail.com>
2023-09-27 16:15:34 +0900

    Updated promotion.jsx

--- src/components/Main/Promotion.jsx
+++ src/components/Main/Promotion.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import * as Styles from './MainStyle';
 
+import data from '../../data/promotion.json'
+
 function Promotion() {
+    const content = [...data]
+
     return (
         <div>
             <section id='promotion'>
@@ -16,6 +20,26 @@ function Promotion() {
                     </div>
                     <div className="slide-container season">
                         <ul className="swiper-wrapper">
+                            {content.map((item, index) => (
+                                <li className="swiper-item" key={index} >
+                                    <a href="#!">
+                                        <div className="season-visual-wrap">
+                                            <div className="season-visual-box">
+                                                <img src={process.env.PUBLIC_URL + item.img } alt={index} />
+                                            </div>
+                                        </div>
+                                        <div className="season-info-wrap">
+                                            <div className="season-text-box">
+                                                <p className="season-title">
+                                                    {item.title}</p>
+                                                <p className="season-subtitle">
+                                                {item.subtitle}</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+
+                            ))}
                             <li className="swiper-item">
                                 <a href="#!">
                                     <div className="season-visual-wrap">

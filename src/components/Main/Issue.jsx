450e06ad4874d566eafce7f7b254cce1e8102ce3
iamjiheehan <hatrix1014@gmail.com>
2023-09-27 17:57:22 +0900

    Updated Issue.jsx

--- src/components/Main/Issue.jsx
+++ src/components/Main/Issue.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-
 import * as Styles from './MainStyle';
-
-import IssueData from '../../data/issue.json'
+import IssueData from '../../data/issue.json';
 
 function Issue() {
     const data = [...IssueData];
@@ -21,7 +19,7 @@ function Issue() {
                                     <li className="swiper-item" key={index}>
                                         <div className="season-visual-wrap">
                                             <a href="#!" className="season-visual-box">
-                                                <img src={process.env.PUBLIC_URL + item.season.img} alt={item.season.img}  />
+                                                <img src={process.env.PUBLIC_URL + item.season.img} alt={item.season.img} />
                                             </a>
                                         </div>
                                         <div className="season-info-wrap">
@@ -35,27 +33,27 @@ function Issue() {
                                                 </a>
                                             </div>
                                             <ul className="product-list">
-                                                {data.map((item, index) => (
-                                                    <li key={index}>
+                                                {item.product.map((product, productIndex) => (
+                                                    <li key={productIndex}>
                                                         <figure className="item-box">
                                                             <div className="item-img">
                                                                 <div className="img-box">
                                                                     <a href="#!">
-                                                                        <div className="img"><img src={process.env.PUBLIC_URL + item.product.img} alt={item.product.img} /></div>
+                                                                        <div className="img"><img src={process.env.PUBLIC_URL + product.img} alt={product.name} /></div>
                                                                     </a>
                                                                 </div>
                                                                 <button type="button" className="btn-like"><span></span></button>
                                                             </div>
                                                             <figcaption className="item-info">
                                                                 <a href="#!">
-                                                                    <div className="item-brand">{item.product.brand}</div>
-                                                                    <div className="item-name">{item.product.name}</div>
+                                                                    <div className="item-brand">{product.brand}</div>
+                                                                    <div className="item-name">{product.name}</div>
                                                                     <div className="item-opt">
                                                                         <span className="item-price">
                                                                             <div className="item-price">
-                                                                                <span className="price">{item.product.price}</span>
-                                                                                <del className="regular">{item.product.regular}</del>
-                                                                                <span className="percent">{item.product.percent}</span>
+                                                                                <span className="price">{product.price}</span>
+                                                                                <del className="regular">{product.regular}</del>
+                                                                                <span className="percent">{product.percent}</span>
                                                                             </div>
                                                                         </span>
                                                                     </div>
@@ -69,6 +67,14 @@ function Issue() {
                                     </li>
                                 ))}
                             </ul>
+                            <div className="slide-nav">
+                                <button className="slide-nav-prev button-disabled">
+                                    <span className="offscreen"></span>
+                                </button>
+                                <button className="slide-nav-next">
+                                    <span className="offscreen"></span>
+                                </button>
+                            </div>
                         </div>
                     </Styles.IssueContent>
                 </Styles.Container>

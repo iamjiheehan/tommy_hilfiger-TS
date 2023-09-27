c53ead33a3fcdd2aec9dcfb0b29ce500d37cd652
iamjiheehan <hatrix1014@gmail.com>
2023-09-27 16:15:57 +0900

    Removed unused html

--- src/components/Main/Promotion.jsx
+++ src/components/Main/Promotion.jsx
@@ -25,7 +25,7 @@ function Promotion() {
                                     <a href="#!">
                                         <div className="season-visual-wrap">
                                             <div className="season-visual-box">
-                                                <img src={process.env.PUBLIC_URL + item.img } alt={index} />
+                                                <img src={process.env.PUBLIC_URL + item.img} alt={index} />
                                             </div>
                                         </div>
                                         <div className="season-info-wrap">
@@ -33,47 +33,12 @@ function Promotion() {
                                                 <p className="season-title">
                                                     {item.title}</p>
                                                 <p className="season-subtitle">
-                                                {item.subtitle}</p>
+                                                    {item.subtitle}</p>
                                             </div>
                                         </div>
                                     </a>
                                 </li>
-
                             ))}
-                            <li className="swiper-item">
-                                <a href="#!">
-                                    <div className="season-visual-wrap">
-                                        <div className="season-visual-box">
-                                            <img src="https://cdn.hfashionmall.com/display/plan/76/10676/10676_1PCE_PC_KOR_20230818160645.jpg?RS=576x576&amp;AR=0&amp;CS=576x576" alt="1" />
-                                        </div>
-                                    </div>
-                                    <div className="season-info-wrap">
-                                        <div className="season-text-box">
-                                            <p className="season-title">
-                                                TOMMY MEN｜ARCHIVE FIT</p>
-                                            <p className="season-subtitle">
-                                                새롭게 선보이는 아카이브핏을 만나보세요</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li className="swiper-item">
-                                <a href="#!">
-                                    <div className="season-visual-wrap">
-                                        <div className="season-visual-box">
-                                            <img src="https://cdn.hfashionmall.com/display/plan/76/10676/10676_1PCE_PC_KOR_20230818160645.jpg?RS=576x576&amp;AR=0&amp;CS=576x576" alt="1" />
-                                        </div>
-                                    </div>
-                                    <div className="season-info-wrap">
-                                        <div className="season-text-box">
-                                            <p className="season-title">
-                                                TOMMY MEN｜ARCHIVE FIT</p>
-                                            <p className="season-subtitle">
-                                                새롭게 선보이는 아카이브핏을 만나보세요</p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
                         </ul>
                         <div className="slide-nav">
                             <button className="slide-nav-prev button-disabled">

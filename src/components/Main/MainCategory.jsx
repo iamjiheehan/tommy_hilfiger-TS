147b0bdae33da05519acd63021b73d3c79ac2579
iamjiheehan <hatrix1014@gmail.com>
2023-10-02 17:39:40 +0900

    Added explanation

--- src/components/Main/MainCategory.jsx
+++ src/components/Main/MainCategory.jsx
@@ -21,7 +21,12 @@ function MainCategory() {
                         <button type="button" onClick={() => trigger(0)} className={active === 0 ? 'on' : ''}><span>CATEGORY</span></button>
                             <button type="button" onClick={() => trigger(1)} className={`${active === 1 ? 'on' : ''} btn_line-before`}><span>DISCOVER</span></button>
                         </Styles.BrandTab>
-                        <Styles.BrandContent className={`brand-line-cont tab-cont2 ${active === 0 ? 'on' : ''}`} active={active === 1}>
+                        <Styles.BrandContent 
+                            className={`brand-line-cont tab-cont2 ${active === 0 ? 'on' : ''}`} 
+                            active={active === 1}>
+                                {/* active={active === 1}: 이 부분은 컴포넌트에 active라는 props를 전달하는 부분 */}
+                                {/* active props는 현재 어떤 탭이 활성화되어 있는지 나타내기 위해 사용됨. */}
+                                {/* active 변수가 1일 때 ('Discover' 탭이 활성화된 상태) active props에 true가 전달됨. */}
                             <ul className="brand-line-list">
                                 {data.people.map((item, index) => (
                                     <li className="brand-line-item" key={index}>

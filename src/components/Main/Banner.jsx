ab7e8639f7d7edd9bfb730d1b9a10c9877406bff
iamjiheehan <hatrix1014@gmail.com>
2023-10-04 10:11:32 +0900

    Update trigger function for pagination

--- src/components/Main/Banner.jsx
+++ src/components/Main/Banner.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as Styles from './MainStyle';
 
-import MainBanner2 from '../../assets/images/main_banner-2.jpg'
-import MainBanner3 from '../../assets/images/main_banner-3.jpg'
+import data from '../../data/banner.json'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from 'swiper/modules';
 
 function Banner() {
+    const dataSet = [...data];
+    const dataLength = dataSet.length;
+
+    console.log(dataLength);
+
+    const [currentSlide, setCurrentSlide] =  useState(1);
+
+    const trigger = () => {
+        let index = currentSlide;
+    
+        if (index < dataLength) {
+            index++;
+            setCurrentSlide(index); 
+            console.log(currentSlide); 
+        }
+    }
+    
+    // const trigger = () => {
+    //     let index = 0;
+    //     if(index < dataLength) {
+    //         setCurrentSlide(index + 1);
+    //         console.log(currentSlide);
+    //     }
+    // }
+    // 기존의 trigger 함수가 작동하지 않았던 
+    // 이유는 index 변수를 올바르게 업데이트하지 않았기 때문. 
+    // trigger 함수 내에서 매번 함수가 호출될 때 index를 0으로 선언했는데, 
+    // 이렇게 하면 항상 0의 값을 가지게 되어 setCurrentSlide(index + 1) 라인은 
+    // 항상 currentSlide를 증가시키지 않는다.
+
+    // 이 문제를 해결하려면 trigger 함수에서 다음 인덱스를 계산하고, 
+    // 그를 기반으로 상태를 업데이트해야 함.
+
+
     return (
         <div>
             <section id='mainBanner'>
@@ -21,51 +54,39 @@ function Banner() {
                             navigation={true}
                             modules={[Pagination, Navigation]}
                             className="mySwiper swiper-wrapper"
-                            onSlideChange={() => console.log('slide change')}
+                            onSlideChange={()=>{
+                                console.log('slide change'); 
+                                trigger();
+                            }}
                             onSwiper={(swiper) => console.log(swiper)}
                         >
-                            <SwiperSlide className='swiper-item'>
-                                <Styles.Box>
-                                    <Styles.Banner className="img">
-                                        <a href="#!">
-                                            <img src={MainBanner3} alt="MainBanner1" />
-                                        </a>
-                                    </Styles.Banner>
-                                    <Styles.Text className="str" href="#!">
-                                        <span className="etc">
-                                            TOMMY HILFIGER X TOMMY JEANS
-                                        </span>
-                                        <strong className="title">가을 니트 맛집, 타미힐피거</strong>
-                                        <span className="sub">ALL-TIME ESSENTIALS</span>
-                                        <Styles.ViewMore><span className='view-text'>VIEW MORE</span></Styles.ViewMore>
-                                    </Styles.Text>
-                                </Styles.Box>
-                            </SwiperSlide>
-                            <SwiperSlide className='swiper-item'>
-                                <Styles.Box>
-                                    <Styles.Image>
-                                        <a href="#!">
-                                            <img src={MainBanner2} alt="MainBanner1" />
-                                        </a>
-                                    </Styles.Image>
-                                    <Styles.Text className="str" href="#!">
-                                        <span className="etc">
-                                            TOMMY HILFIGER X TOMMY JEANS
-                                        </span>
-                                        <strong className="title">가을 니트 맛집, 타미힐피거</strong>
-                                        <span className="sub">ALL-TIME ESSENTIALS</span>
-                                        <Styles.ViewMore><span className='view-text'>VIEW MORE</span></Styles.ViewMore>
-                                    </Styles.Text>
-                                </Styles.Box>
-                            </SwiperSlide>
+                            {data.map((item, index) => (
+                                <SwiperSlide className='swiper-item'>
+                                    <Styles.Box>
+                                        <Styles.Banner className="img">
+                                            <a href="#!">
+                                                <img src={item.img} alt={item.title} />
+                                            </a>
+                                        </Styles.Banner>
+                                        <Styles.Text className="str" href="#!">
+                                            <span className="etc">
+                                                {item.etc}
+                                            </span>
+                                            <strong className="title">{item.title}</strong>
+                                            <span className="sub">{item.sub}</span>
+                                            <Styles.ViewMore><span className='view-text'>VIEW MORE</span></Styles.ViewMore>
+                                        </Styles.Text>
+                                    </Styles.Box>
+                                </SwiperSlide>
+                            ))}
                         </Swiper>
                     </div>
                     <div className="swiper-tool-wrap">
                         <div className="swiper-tool">
                             <div className="swiper-pagination">
-                                <span className="swiper-pagination-current">2</span>
+                                <span className="swiper-pagination-current">{currentSlide}</span>
                                 <span className='swiper-pagination-bar'>/</span>
-                                <span className="swiper-pagination-total">9</span>
+                                <span className="swiper-pagination-total">{dataLength}</span>
                             </div>
                         </div>
                     </div>

ee0ac098fc4220046e34ac44e2a4ad12557c32b6
한지희, Jihee Han <hatrix1014@gmail.com>
2023-10-10 01:08:36 +0900

    Updated BreadCrumb for product Main

--- src/components/ProductMain/ProductMain.jsx
+++ src/components/ProductMain/ProductMain.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 
 import * as Styles from './ProductMainStyle'
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
 export default function ProductMain() {
     return (
         <>
             <Styles.Container id='productMain'>
-                {/* <h3>야</h3>
-                <Breadcrumb>
-                    <Breadcrumb.Item href="#!" className='bread-item'>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item href="#!">
-                        Library
-                    </Breadcrumb.Item>
-                    <Breadcrumb.Item active>Data</Breadcrumb.Item>
-                </Breadcrumb> */}
+                <div className="breadcrumb-wrap">
+                    <ol className="breadcrumb">
+                        <li className="bc-home"><a href="/">HOME</a></li>
+                        <li>
+                            <a href="#!">
+                                TOMMY HILFIGER MEN</a>
+                        </li>
+                        <li className="">
+                            <a href="#!">
+                                남성</a>
+                        </li>
+                        <li className="">
+                            <a href="#!">
+                                아우터</a>
+                        </li>
+                        <li className="active">
+                            다운/패딩</li>
+                    </ol>
+                </div>
                 <h3>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias magni repellat illo, veniam dolorum quia facilis labore nulla eius assumenda tempore saepe eligendi voluptatum soluta cupiditate nisi sunt quidem mollitia!
                 </h3>
--- src/components/ProductMain/ProductMainStyle.js
+++ src/components/ProductMain/ProductMainStyle.js
@@ -1,21 +1,67 @@
 import styled from "styled-components";
+import breadcrumb from "../../assets/images/icon_breadcrumb.png"
+
 
 export const Container = styled.div`
-    background-color: #FFF;
+    background-color: #fff;
     /* height: 500px; */
-        min-width: 1280px;
-        max-width: 1920px;
-        padding: 130px 80px 160px;
-        margin: 0 auto;
+    min-width: 1280px;
+    max-width: 1920px;
+    padding: 140px 80px 160px;
+    margin: 0 auto;
 
-    .bread-item {
-        color: black !important;
-        z-index: 999;
+    a {
+        color: inherit;
+        font-size: inherit;
     }
 
+    .breadcrumb-wrap .breadcrumb {
+        display: -webkit-box;
+        display: -moz-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-align: center;
+        -moz-box-align: center;
+        -ms-flex-align: center;
+        -webkit-align-items: center;
+        align-items: center;
+
+        height: 100%;
+        margin: 0 auto;
+    }
+
+    .breadcrumb-wrap .breadcrumb li {
+list-style: none;
+
+        position: relative;
+    min-height: 14px;
+    color: #999;
+    font-size: 13px;
+    font-weight: 300;
+
+    }
+    .breadcrumb-wrap .breadcrumb li.active {
+        color: #000 !important;
+    }
+    .breadcrumb-wrap .breadcrumb li + li {
+        margin-left: 12px;
+        padding-left: 19px;
+    }
+
+    .breadcrumb-wrap .breadcrumb li + li:before {
+    content: '';
+    display: inline-block;
+    position: absolute;
+    top: 3px;
+    left: 0;
+    width: 5px;
+    height: 9px;
+    background: url(${breadcrumb}) no-repeat 0 0;
+    vertical-align: top;
+}
+    
     h3 {
         font-size: 50px;
     }
 `;
-
-

31399916bdcb55a172971e78a9e8845acfa5ec80
iamjiheehan <hatrix1014@gmail.com>
2023-10-10 11:10:22 +0900

    [Updated]Update on Process

--- src/components/ProductMain/ProductMain.jsx
+++ src/components/ProductMain/ProductMain.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import * as Styles from './ProductMainStyle'
 
+import data from '../../data/Product.json'
+
 export default function ProductMain() {
     return (
         <>
@@ -25,10 +27,56 @@ export default function ProductMain() {
                             다운/패딩</li>
                     </ol>
                 </div>
-                <div className="resposive-wrap">
+                <div className="responsive-wrap">
                     <div className="view-top">
-                        <div className="top-left"></div>
-                        <div className="top-right"></div>
+                        <div className="top-left">
+                            <div className="left-swiper">
+                                <div className="swiper-container">
+                                    <div className="swiper-wrapper">
+                                        <div className="swiper-slide">
+                                            <img src="https://cdn.hfashionmall.com/goods/THBR/23/09/22/GM0123092244419_4_ORGINL_1695373248443.jpg?RS=960x960&AR=0&CS=640x960" alt="" />
+                                        </div>
+                                        <div className="swiper-slide">
+                                            <img src="https://cdn.hfashionmall.com/goods/THBR/23/09/22/GM0123092244419_5_ORGINL_1695373248599.jpg?RS=960x960&AR=0&CS=640x960" alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <Styles.TopRight className='top-right'>
+                            <div className="etc-btn">
+                                <button type="button" class="btn-like"><span></span></button>
+                                <span class="bar">|</span>
+                                <button type="button" class="btn-share"><span></span></button>
+                            </div>
+                            <p className="item-brand">
+                                <a href="#!">
+                                    TOMMY HELFIGER MEN
+                                </a>
+                            </p>
+                            <p className="item-tag">
+                                <span className="code">T12D9QOT020MT1RBN</span>
+                                <span className="tag" style={{ color: "#C2935F" }}>
+                                </span>
+                            </p>
+                            <p className="item-name">뉴욕 가먼트다이 덕다운 푸퍼</p>
+                            <div className="item-price">
+                                <p class="price"><span>579,000</span></p>
+                                <p class="coupon">
+                                    <span class="num">
+                                        521,100</span>
+                                    <span class="txt">회원/멤버십 쿠폰가
+                                        <button type="button" class="btn-tooltip">툴팁보기</button>
+                                    </span>
+                                    <button type="button" class="btn-coupon"><span>쿠폰다운로드</span>
+                                    </button>
+                                </p>
+                            </div>
+                            <Styles.ViewOption className="view-option">
+                                
+                            </Styles.ViewOption>
+                            <div className="info-bot"></div>
+                        </Styles.TopRight>
                     </div>
                     <div className="view-list"></div>
                     <div className="view-detail">
--- src/components/ProductMain/ProductMainStyle.js
+++ src/components/ProductMain/ProductMainStyle.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import breadcrumb from "../../assets/images/icon_breadcrumb.png";
 
+import linkarrow from "../../assets/images/icon_link_arrow2.png";
+import like from "../../assets/images/btn_like_sub.png";
+import share from "../../assets/images/icon-share-sub.png";
+
 export const Container = styled.div`
     background-color: #fff;
     /* height: 500px; */
@@ -8,6 +12,8 @@ export const Container = styled.div`
     max-width: 1920px;
     padding: 140px 80px 160px;
     margin: 0 auto;
+
+
 
     a {
         color: inherit;
@@ -59,6 +65,174 @@ export const Container = styled.div`
         vertical-align: top;
     }
 
-    h3 {
+
+    .view-top {
+        position: relative;
+        display: table;
+        width: 100%;
+        table-layout: fixed;
     }
+
+    .top-left {
+        display: table-cell;
+        position: relative;
+    }
+
+    .swiper-slide {
+        width: 50%;
+        flex: 0 0 auto;
+
+        flex-shrink: 1 !important;
+    }
+`;
+
+export const TopRight = styled.div`
+    display: table-cell;
+    position: relative;
+    vertical-align: top;
+    z-index: 2;
+    width: 460px;
+    padding-left: 80px;
+    background: #fff;
+
+    span {
+        font-size:inherit;
+    }
+
+    .item-brand {
+        margin: 30px 0 70px;
+        font-weight: 300;
+    }
+
+    .item-brand a {
+        padding-right: 14px;
+        background: url(${linkarrow}) no-repeat 100% 50%;
+    }
+
+    .etc-btn {
+        position: absolute;
+        top: 30px;
+        right: 0;
+
+        .bar {
+            display: inline-block;
+            margin: 0 10px;
+            height: 18px;
+            vertical-align: middle;
+            font-size: 0;
+            border-left: 1px solid #eee;
+        }
+    }
+
+    .btn-like::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: -25px;
+        right: 0;
+        display: block;
+        width: 23px;
+        height: 21px;
+        background: url(${like}) no-repeat;
+        opacity: 0.3;
+        transition: opacity .3s;
+    }
+    .btn-share {
+        font-size: 0;
+        width: 17px;
+        height: 19px;
+        background: url(${share}) no-repeat;
+    }
+
+
+    .item-tag {
+        margin-bottom: 10px;
+    }
+
+    .item-name {
+        font-size: 30px;
+        font-weight: 700;
+        line-height: 38px;
+    }
+
+    .product-view-top .item-price {
+        margin: 10px 0 0;
+    }
+
+    .item-price .price {
+        font-size: 30px;
+    }
+
+    .item-price .coupon .num {
+        display: inline-block;
+        margin: 5px 0;
+        font-size: 30px;
+        color: #c2935f;
+        margin-right: 5px;
+    }
+
+    .coupon .txt {
+        display: inline-block;
+        margin-left: 0;
+        margin-right: 7px;
+        vertical-align: middle;
+        font-size: 14px;
+
+        .btn-tooltip {
+            display: inline-block;
+            width: 18px;
+            height: 18px;
+            background: #eee;
+            font-size: 0;
+            vertical-align: middle;
+            border-radius: 2px;
+
+            margin-bottom: 3px;
+        }
+        :before {
+            content: "?";
+            display: inline-block;
+            font-size: 12px;
+            color: #666;
+        }
+    }
+
+    .coupon .btn-coupon {
+        line-height: 24px;
+        height: 26px;
+        margin-left: 0;
+        vertical-align: middle;
+
+        min-width: 90px;
+        height: 30px;
+        padding: 0 10px;
+        line-height: 28px;
+        font-size: 12px;
+        border-radius: 2px;
+
+        color: #fff !important;
+        background: #333;
+    }
+
+    .item-price .coupon > * {
+        line-height: 30px;
+        vertical-align: middle;
+    }
+
+    [class*="btn-type"] span {
+        position: relative;
+    }
+`;
+
+export const ViewOption = styled.div`
+    margin-top: 40px;
+    padding-top: 40px;
+    border-top: 1px solid #eee;
+
+    .color {
+        position: relative;
+        margin-top: -10px;
+        padding-right: 115px;
+}
+
 `;

090f3e7b9119a3cee0cb69babe107b03bde897a5
iamjiheehan <hatrix1014@gmail.com>
2023-10-13 17:55:58 +0900

    [Updated] Added Id for golf.json of NewIn data

--- src/components/Main/ItemLayout.jsx
+++ src/components/Main/ItemLayout.jsx
@@ -7,6 +7,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation} from 'swiper/modules';
 import SwiperCore from 'swiper/core';
 
+import { Link } from 'react-router-dom';
+
 function ItemLayout({ items, params }) {
 
     const [swiper, setSwiper] = useState(null);
@@ -20,33 +22,35 @@ function ItemLayout({ items, params }) {
         >
             {items.map((item, index) => (
                 <SwiperSlide className="swiper-item" key={index}>
-                    <figure className="item-box" >
-                        <div className="item-img">
-                            <div className="img-box">
+                    <Link key={index} to={`/product/${index}/detail`}>
+                        <figure className="item-box" >
+                            <div className="item-img">
+                                <div className="img-box">
+                                    <a href="#!">
+                                        <div className="img">
+                                            <img src={process.env.PUBLIC_URL + item.img} alt={item.name} />
+                                        </div>
+                                    </a>
+                                </div>
+                                <button type="button" className="btn-like"></button>
+                                <button type="button" className="btn-quick"></button>
+                            </div>
+                            <figcaption className="item-info">
                                 <a href="#!">
-                                    <div className="img">
-                                        <img src={process.env.PUBLIC_URL + item.img} alt={item.name} />
+                                    <div className="item-brand">{item.brand}</div>
+                                    <div className="item-name">{item.name}</div>
+                                    <div className="item-opt">
+                                        <div className="item-price">
+                                            <span className="price">{item.price}</span>
+                                            <del className="regular">{item.regular}</del>
+                                            <span className="percent">{item.percent}</span>
+                                        </div>
+                                        <div className="item-label"></div>
                                     </div>
                                 </a>
-                            </div>
-                            <button type="button" className="btn-like"></button>
-                            <button type="button" className="btn-quick"></button>
-                        </div>
-                        <figcaption className="item-info">
-                            <a href="#!">
-                                <div className="item-brand">{item.brand}</div>
-                                <div className="item-name">{item.name}</div>
-                                <div className="item-opt">
-                                    <div className="item-price">
-                                        <span className="price">{item.price}</span>
-                                        <del className="regular">{item.regular}</del>
-                                        <span className="percent">{item.percent}</span>
-                                    </div>
-                                    <div className="item-label"></div>
-                                </div>
-                            </a>
-                        </figcaption>
-                    </figure>
+                            </figcaption>
+                        </figure>
+                    </Link>
                 </SwiperSlide>
             ))}
             

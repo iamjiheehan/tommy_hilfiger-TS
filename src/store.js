fa0b6e973797f7cee8a513f854dd0a5852ce6774
iamjiheehan <hatrix1014@gmail.com>
2023-10-18 18:12:52 +0900

    [Updated] Set up Defaul value to store.js

--- src/store.js
+++ src/store.js
@@ -3,8 +3,49 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 // Cart slice
 const cart = createSlice({
     name: "cart",
-    initialState: { items: [
-    ] },
+    initialState: {
+        items: [
+            {
+                "id": "NT002",
+                "brand": "TOMMY JEANS",
+                "name": "케이블 집업",
+                "price": "210,000",
+                "regular": "248,800",
+                "percent": "15%",
+                "img": "img/NewIn/Jeans/002.jpg",
+                "imgSub":"img/NewIn/Jeans/002-1.jpg",
+                "category": "니트/스웨터",
+                "gender":"여성",
+                "style":"가디건"
+            },
+            {
+                "id": "NT003",
+                "brand": "TOMMY JEANS",
+                "name": "[ALASKA] 크롭 푸퍼",
+                "price": "304,300",
+                "regular": "358,000",
+                "percent": "15%",
+                "img": "img/NewIn/Jeans/003.jpg",
+                "imgSub":"img/NewIn/Jeans/003-1.jpg",
+                "category": "아우터",
+                "gender":"여성",
+                "style":"패딩"
+            },
+            {
+                "id": "NT004",
+                "brand": "TOMMY JEANS",
+                "name": "소피 스트레이트핏 미드블루 데님",
+                "price": "159,800",
+                "regular": "188,000",
+                "percent": "15%",
+                "img": "img/NewIn/Jeans/004.jpg",
+                "imgSub":"img/NewIn/Jeans/004-1.jpg",
+                "category": "팬츠",
+                "gender":"여성",
+                "style":"데님"
+            },
+        ],
+    },
     reducers: {
         reset: () => ({ items: [] }),
 
